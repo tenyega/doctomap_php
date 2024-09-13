30322eb7d49709cdc51203b22d18897748c48291
yega <mdolma@ymail.com>
2024-09-13 10:41:24 +0200

    added data with fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Doctor;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        $specialisations_medicales = [
+            "Cardiologie",
+            "Dermatologie",
+            "Neurologie",
+            "Pédiatrie",
+            "Oncologie",
+            "Psychiatrie",
+            "Chirurgie générale",
+            "Gynécologie-obstétrique",
+            "Ophtalmologie",
+            "Anesthésiologie"
+        ];
+        for ($i = 0; $i < 30; $i++) {
+            $doctor = new Doctor();
+            $doctor->setFirstname($faker->firstName());
+            $doctor->setLastname($faker->lastName());
+            $doctor->setSpeciality($faker->randomElement($specialisations_medicales));
+            $doctor->setAddress($faker->streetAddress());
+            $doctor->setCity($faker->city());
+            $doctor->setZip($faker->postcode());
+            $doctor->setPhone($faker->phoneNumber());
+
+            $manager->persist($doctor);
+        }
+        $manager->flush();
+    }
+}
